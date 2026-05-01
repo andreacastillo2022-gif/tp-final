bbe4e66115da71a504ed67c6d8bebc598511308d
Andrea Agustina Castillo <andreacastillo2022@gmail.com>
2026-05-01 16:09:26 -0300

    mongo conectado

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
-const PORT = 3000;
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
